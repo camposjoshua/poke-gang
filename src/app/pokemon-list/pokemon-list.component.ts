ea53dc4e888b946657f4b91270532b7245942db8
unknown <jesrey.suazo@gmail.com>
2023-01-06 20:52:03 +0800

    pokedex color type

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -8,22 +8,46 @@ import { DataService } from '../services/data.service';
 })
 export class PokemonListComponent {
   pokemons: any[] = [];
+  testArr : any;
 
   constructor(
     private dataService: DataService
-  ) { }
+  ) {
+    this.dataService.getPokemons()
+    .subscribe((response: any) => {
+      response.results.forEach((result: any) => {
+        this.dataService.getMoreData(result.name)
+          .subscribe((uniqResponse: any) => {
+            this.pokemons.push(uniqResponse);
+          });
+      })
+    });
+   }
 
   ngOnInit(): void {
-    this.dataService.getPokemons()
-      .subscribe((response: any) => {
-        response.results.forEach((result: any) => {
-          this.dataService.getMoreData(result.name)
-            .subscribe((uniqResponse: any) => {
-              this.pokemons.push(uniqResponse);
-              console.log(this.pokemons)
-            });
-        })
-      });
+    console.log(this.pokemons);
+      
+  }
+
+  getColor(index:number){
+    const typeArr = this.pokemons[index].types;
+    let color = 'black';
+    for (let typeIndex = 0; typeIndex < typeArr.length; typeIndex++) {
+      // console.log(this.pokemons[index].name ,typeArr[typeIndex]);
+      if(this.pokemons[index].types[typeIndex].type.name === 'grass'){
+        color = 'green';
+      }
+      if(this.pokemons[index].types[typeIndex].type.name === 'fire'){
+        color = 'red';
+      }
+      if(this.pokemons[index].types[typeIndex].type.name === 'water'){
+        color = 'blue';
+      }
+      if(this.pokemons[index].types[typeIndex].type.name === 'flying'){
+        color = 'gray';
+      }
+    }
+    return color;
   }
 
 }
